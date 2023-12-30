eafdc9413ab958534bfbf55d5a192ea4a2ab3f90
Ryan Mugo <gathariryan@gmail.com>
2023-12-30 17:00:23 +0100

    Backend and frontend preparation for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,11 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import morgan from "morgan";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/userRoutes";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 mongoose.connect(process.env.MONGO_URI as string);
 
@@ -19,7 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
 app.use("/api/users", userRoutes);
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on localhost:5000");
