357d88a360d2d93c4ab1fa89ce39c1e8acbe74f4
Ryan Mugo <gathariryan@gmail.com>
2023-12-31 15:38:41 +0100

    Update of tests..was a slight error found but i have fixed it

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -11,9 +11,9 @@ test.beforeEach(async ({ page }) => {
 
   await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
 
-  await page.locator("[name=email]").fill("ryangathari254@gmail.com");
+  await page.locator("[name=email]").fill("bas@gmail.com");
 
-  await page.locator("[name=password]").fill("ryandegea1");
+  await page.locator("[name=password]").fill("decks123");
 
   await page.getByRole("button", { name: "Login" }).click();
 
@@ -35,16 +35,18 @@ test("should allow user to add a hotel", async ({ page }) => {
   await page.getByText("Budget").click();
 
   await page.getByLabel("Free Wifi").check();
-  await page.getByLabel("Parking").check();
+  await page.getByLabel("Family Rooms").check();
+  await page.getByLabel("Outdoor Pool").check();
+  await page.getByLabel("Spa").check();
 
   await page.locator('[name="adultCount"]').fill("2");
-  await page.locator('[name="childCount"]').fill("4");
+  await page.locator('[name="childCount"]').fill("0");
 
   await page.setInputFiles('[name="imageFiles"]', [
-    path.join(__dirname, "files", "1.jpg"),
     path.join(__dirname, "files", "2.jpg"),
     path.join(__dirname, "files", "3.jpg"),
     path.join(__dirname, "files", "4.jpg"),
+    path.join(__dirname, "files", "1.jpg"),
   ]);
 
   await page.getByRole("button", { name: "Save" }).click();
@@ -56,14 +58,13 @@ test("should display hotels", async ({ page }) => {
   await page.goto(`${UI_URL}my-hotels`);
 
   await expect(page.getByText("Shinyalu Islands")).toBeVisible();
-
   await expect(
-    page.locator(':has-text("Welcome to our opulent sanctuary")')
+    page.getByText("Welcome to our opulent sanctuary")
   ).toBeVisible();
 
   await expect(page.getByText("Kakamega, Kenya")).toBeVisible();
   await expect(page.getByText("All Inclusive")).toBeVisible();
-  await expect(page.getByText("$60 per night")).toBeVisible();
+  await expect(page.getByText("$78 per night")).toBeVisible();
   await expect(page.getByText("2 adults, 0 children")).toBeVisible();
   await expect(page.getByText("5 Star Rating")).toBeVisible();
 
